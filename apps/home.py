14f1622a34caff77751a6ed799b7b4f4eb2c4ed6
charles940 <84117699+charles940@users.noreply.github.com>
2022-02-05 00:49:24 +0100

    Add files via upload

---
+++ apps/home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def app():
+    st.title('Home')
+
+    st.write('Bienvenue sur la page de prevision financiere .')
+
+    st.write('avant toute action veuillez vous connecter ou vous inscrire.')
